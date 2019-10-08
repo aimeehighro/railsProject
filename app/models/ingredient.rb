4ede1da875bb180071ca532f48938d53e71f549c
Aimee <mmalaluan6@academic.rrc.ca>
2019-10-07 23:39:22 -0500

    Added validations and association to the Ingredient model

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,2 +1,8 @@
+# frozen_string_literal: true
+
 class Ingredient < ApplicationRecord
+  has_many :measurements
+  has_many :dishes, through: :measurements
+  validates :name, presence: true
+  validates :name, uniqueness: true
 end
