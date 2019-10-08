e9dfccf7e756f3aaa95efaceef7458af1285f03d
Aimee <mmalaluan6@academic.rrc.ca>
2019-10-07 23:46:19 -0500

    Added Validations and Associations to the Dish Model

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,2 +1,8 @@
 class Dish < ApplicationRecord
+  has_many :people
+  has_many :measurements
+  has_many :ingredients, through: :measurements
+
+  validates :name, :description, presence: true
+  validates :name, uniqueness: true
 end
