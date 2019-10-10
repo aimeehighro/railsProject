68507be496a06642f08b3a824642296fa50691c9
Aimee <mmalaluan6@academic.rrc.ca>
2019-10-10 11:09:56 -0500

    Added the index, show and root routes (Dishes)

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  get 'ingredients/index'
-  get 'ingredients/show'
-  get 'measurements/show'
-  get 'people/show'
-  get 'dishes/index'
-  get 'dishes/show'
+  # get 'ingredients/index'
+  # get 'ingredients/show'
+  # get 'measurements/show'
+  # get 'people/show'
+  # get 'dishes/index'
+  # get 'dishes/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get '/dishes', to: 'dishes#index'
+  get 'dishes/:id', to: 'dishes#show', id: /\d+/
+  root to: 'dishes#index'
 end
